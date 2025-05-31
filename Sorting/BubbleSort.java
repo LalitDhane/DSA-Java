9aa5aa0712c028b78df86b9f70a2452e22b111be
Lalit Dhane <lalitdhane4455@gmail.com>
2025-05-31 20:50:45 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,22 +1,29 @@
+import java.util.Arrays;
+
 public class BubbleSort {
     //Time Complexity O(n^2)
     //Stable Sort Algorithm 
+    
     public static void main(String[] args) {
         int[] arr = {4,2,-1,43,9,-3};
-        for (int num : arr) System.out.print(num+" ");
-        System.out.println(); 
+        boolean swapped;
+        System.out.println(Arrays.toString(arr));
 
         for(int i = arr.length - 1; i >= 0; i--) {
+            swapped = false;
             for(int j = 0; j < i; j++) {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
             }
+
+            //break if array is already sorted.
+            if(!swapped) break;
         }
-        
-        for (int num : arr) System.out.print(num+" ");
-        System.out.println();
+
+        System.out.println(Arrays.toString(arr));
     }
 }
